ff90601e46be7abc4d308b6b20cf2b171c9549f8
HKDITO <1263@hakudo.co.jp>
2024-07-29 13:42:55 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('v1').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/app.js',
-          '/icon.png',
-          '/manifest.json'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/icon.png',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
